8fde6a79f593cbec26e80e94145fe8dc70be86ea
Murilo Santos <142051797+murilosantosb@users.noreply.github.com>
2026-08-12 10:19:13 -0300

    Subindo projeto React

---
+++ React/vite-project/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+
+// Components
+import Header from './components/Header/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <section>
+        <article>
+          <h1>lorem ipsum</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur nisi earum numquam ut voluptatibus assumenda incidunt blanditiis? Unde eaque quis laborum. Fugiat ratione, quae fugit repellendus rerum minus eius blanditiis?</p>
+        </article>
+
+        <article >
+          <h1>lorem ipsum</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur nisi earum numquam ut voluptatibus assumenda incidunt blanditiis? Unde eaque quis laborum. Fugiat ratione, quae fugit repellendus rerum minus eius blanditiis?</p>
+        </article>
+
+        
+
+      </section>
+      <hr />
+
+      <footer>
+        <p>Copyright &copy; 2026</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
---
+++ React/vite-project/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./Header.module.css";
+
+const Header = () => {
+
+  return (
+    <header>
+      <h1 className="title">Header</h1>
+    </header>
+  )
+}
+
+export default Header
